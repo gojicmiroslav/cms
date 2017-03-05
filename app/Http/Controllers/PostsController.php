f2aae2c3572ec029dacbc8e020f97c1c639fd577
gojicmiroslav <miroslav.gojic78@gmail.com>
2017-03-05 01:52:52 +0100

    Post index action

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,10 @@
 @section('content')
 	<h1>All Posts</h1>
 
+	@foreach($posts as $post)
+		<p>{{ $post->title }}</p>
+	@endforeach
+
 @endsection
 
 @section('footer')
